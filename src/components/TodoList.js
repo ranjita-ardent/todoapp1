2da377e7ee6182f3232e79f6fff6b175e79c5093
ranjita-ardent <ranjita.naik@ardentisys.com>
2025-02-17 13:21:19 +0530

    Update TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,48 +1,33 @@
-import React, { useEffect } from 'react';
-import { FaTrash } from 'react-icons/fa'; // Import trash icon
+import React from 'react';
+import { FaTrash } from 'react-icons/fa';
 
 function TodoList({ section, todos, onClearCompleted, onTodoClick, onDeleteClick, isSectionEmpty, isTodoCleared }) {
   const completedTasks = todos.filter(todo => todo.completed);
   const isTodoListEmpty = todos.length === 0;
 
-  useEffect(() => {
-    // Hide the todo container if the section is empty when we switch sections
-    const todoContainer = document.querySelector('.todo-container');
-    
-    if (isSectionEmpty && !isTodoCleared) {
-      todoContainer.classList.remove('visible'); // Hide if no todos
-    } else {
-      todoContainer.classList.add('visible'); // Show if there are todos or completed tasks
-    }
-  }, [isSectionEmpty, isTodoCleared]);
-
   return (
     <div className={`todo-container ${(!isTodoListEmpty || isTodoCleared) ? 'visible' : ''}`}>
       {/* Loop through each todo */}
       {todos.map((todo, index) => (
-        <div key={index} className={`todo-item ${todo.completed ? 'completed' : ''}`}>
+        <div key={todo.id} className={`todo-item ${todo.completed ? 'completed' : ''}`}>
           {/* Radio Button to toggle completion */}
           <input
             type="radio"
             checked={todo.completed}
-            onChange={() => onTodoClick(todo.todoText)} // Toggle completion
+            onChange={() => onTodoClick(todo.id)} // Use the unique id
           />
-
           {/* Todo Text */}
           <span
             className={`todo-text ${todo.completed ? 'line-through' : ''}`}
-            onClick={() => onTodoClick(todo.todoText)} // Toggle completion on text click as well
+            onClick={() => onTodoClick(todo.id)} // Use the unique id
           >
             {todo.todoText}
           </span>
-
           {/* Trash Icon to delete the todo */}
           <FaTrash
             className="trash-icon"
-            onClick={() => onDeleteClick(todo.todoText)} // Delete the todo
+            onClick={() => onDeleteClick(todo.id)} // Use the unique id
           />
-
-          {/* Blue horizontal line below each todo item (block level) */}
           <div className="todo-line"></div>
         </div>
       ))}
