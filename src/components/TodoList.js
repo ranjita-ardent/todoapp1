b327963b01fee34cfcacaab08caf7be240fe1f8b
ranjita-ardent <ranjita.naik@ardentisys.com>
2025-02-17 17:58:35 +0530

    Update TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,39 +1,40 @@
 import React from 'react';
-import { FaTrash } from 'react-icons/fa';
+import { FaTrash } from 'react-icons/fa'; // Import trash icon
 
-function TodoList({ section, todos, onClearCompleted, onTodoClick, onDeleteClick, isSectionEmpty, isTodoCleared }) {
+function TodoList({ section, todos, onClearCompleted, onTodoClick, onDeleteClick, isSectionEmpty }) {
   const completedTasks = todos.filter(todo => todo.completed);
   const isTodoListEmpty = todos.length === 0;
 
   return (
-    <div className={`todo-container ${(!isTodoListEmpty || isTodoCleared) ? 'visible' : ''}`}>
-      {/* Loop through each todo */}
+    <div className={`todo-container ${(!isTodoListEmpty || completedTasks.length > 0) ? 'visible' : ''}`}>
       {todos.map((todo, index) => (
         <div key={todo.id} className={`todo-item ${todo.completed ? 'completed' : ''}`}>
           {/* Radio Button to toggle completion */}
           <input
             type="radio"
             checked={todo.completed}
-            onChange={() => onTodoClick(todo.id)} // Use the unique id
+            onChange={() => onTodoClick(todo.id)} // Pass the ID for toggling completion
           />
+
           {/* Todo Text */}
           <span
             className={`todo-text ${todo.completed ? 'line-through' : ''}`}
-            onClick={() => onTodoClick(todo.id)} // Use the unique id
+            onClick={() => onTodoClick(todo.id)} // Pass the ID for toggling completion
           >
             {todo.todoText}
           </span>
+
           {/* Trash Icon to delete the todo */}
           <FaTrash
             className="trash-icon"
-            onClick={() => onDeleteClick(todo.id)} // Use the unique id
+            onClick={() => onDeleteClick(todo.id)} // Pass the ID for deleting the todo
           />
+
           <div className="todo-line"></div>
         </div>
       ))}
 
-      {/* Show Clear Completed Tasks button if there are completed tasks or any tasks */}
-      {(completedTasks.length > 0 || !isTodoListEmpty || isTodoCleared) && (
+      {(completedTasks.length > 0 || !isTodoListEmpty) && (
         <div className="clear-completed-container">
           <div className="clear-completed" onClick={onClearCompleted}>
             Clear Completed Tasks
