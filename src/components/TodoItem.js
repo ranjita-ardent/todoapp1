deb033fe3867e60968872492305dc75b2ee7ff63
ranjita-ardent <ranjita.naik@ardentisys.com>
2025-02-17 17:58:59 +0530

    Update TodoItem.js

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,19 +2,17 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import { useDispatch } from 'react-redux';
-import { deleteTodo, toggleTodoCompletion } from '../features/todosSlice';
+import { deleteTodo, markCompleted } from '../features/todosSlice';
 
-const TodoItem = ({ todo, section }) => {
+const TodoItem = ({ todo }) => {
   const dispatch = useDispatch();
 
-  // Handle radio button click to toggle completion status
-  const handleComplete = () => {
-    dispatch(toggleTodoCompletion({ id: todo.id, section })); // Pass the unique ID
+  const handleDelete = () => {
+    dispatch(deleteTodo({ id: todo.id }));
   };
 
-  // Handle delete button click to delete the todo from the current section
-  const handleDelete = () => {
-    dispatch(deleteTodo({ id: todo.id, section })); // Pass the unique ID
+  const handleComplete = () => {
+    dispatch(markCompleted({ id: todo.id }));
   };
 
   return (
@@ -25,12 +23,7 @@ const TodoItem = ({ todo, section }) => {
         checked={todo.completed}
         onChange={handleComplete}
       />
-      <label
-        className={`todo-text ${todo.completed ? 'line-through' : ''}`}
-        onClick={handleComplete}
-      >
-        {todo.todoText}
-      </label>
+      <label>{todo.todoText}</label>
       <FontAwesomeIcon
         icon={faTrashAlt}
         className="todo-delete"
